a44971f17e4bcb1ee5b869c48ed6c3d59351a251
Dicrit <Dima.Bolsunov@gmail.com>
2018-11-22 16:13:50 +0200

    moved out convertion logic into separate class

--- GPX_to_txt_converter/Program.cs
+++ GPX_to_txt_converter/Program.cs
@@ -1,63 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml;
 
 
 namespace GPX_to_txt_converter
 {
     class Program
     {
-        static IEnumerable<string> GetAllWpts(string filePath)
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(filePath);
-            XmlNode gpx = doc.DocumentElement;
-            foreach (XmlNode wpt in gpx.ChildNodes)
-            {
-                string lat = wpt.Attributes["lat"].Value;
-                string lon = wpt.Attributes["lon"].Value;
-                string name = wpt["name"].InnerText;
-                string result = string.Format("{0},{1},{2}", name, lat, lon);
-                yield return result;
-            }
-        }
-
-        static void ConvertGpx(string inputFilePath, string outputFilePath)
-        {
-            if (!File.Exists(inputFilePath))
-            {
-                Console.WriteLine("FAILED, file {0} doesn't exist", inputFilePath);
-                return;
-            }
-            int current = 0;
-            StreamWriter outputFile = File.CreateText(outputFilePath);
-            foreach (string str in GetAllWpts(inputFilePath))
-            {
-                outputFile.WriteLine(str);
-                if (current > 0)
-                {
-                    Console.CursorTop = Console.CursorTop - 1;
-                }
-                Console.WriteLine("Done {0}", ++current);
-            }
-            outputFile.Close();
-            Console.WriteLine("Finished.");
-        }
-
-        static void ConvertGpx(string inputPath)
-        {
-            string directory = new FileInfo(inputPath).Directory.FullName;
-            string fileName = Path.GetFileNameWithoutExtension(inputPath);
-            string outputPath = string.Format("{0}\\{1}{2}", directory, fileName, ".txt");
-            ConvertGpx(inputPath, outputPath);
-        }
+        
 
         static void Main(string[] args)
         {
             if (args.Length == 1)
             {
-                ConvertGpx(args[0]);
+                GPXConverter.ConvertGpx(args[0]);
                 return;
             }
 
@@ -65,7 +19,7 @@ namespace GPX_to_txt_converter
             Console.WriteLine("Enter input path:");
             inputPath = Console.ReadLine();
             inputPath = inputPath.Trim('\"');
-            ConvertGpx(inputPath);
+            GPXConverter.ConvertGpx(inputPath);
 
 
             Console.WriteLine("press any key to continue...");
