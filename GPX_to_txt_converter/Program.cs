ff08cf4c1f511797c6b0c384826f83e4675a4500
Dicrit <Dima.Bolsunov@gmail.com>
2018-11-22 17:19:36 +0200

    now ask for file with open file dialog

--- GPX_to_txt_converter/Program.cs
+++ GPX_to_txt_converter/Program.cs
@@ -1,29 +1,68 @@
 ﻿using System;
-
+using System.Windows.Forms;
 
 namespace GPX_to_txt_converter
 {
     class Program
     {
-        
+        static string OpenDialog()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "gpx files (*.gpx)|*.gpx|All files (*.*)|*.*";
 
+                DialogResult result = openFileDialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    return openFileDialog.FileName;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    throw new Exception("");
+                }
+                else
+                {
+                    throw new Exception("Failed to get filename from dialog");
+                }
+            }
+        }
+
+        static bool AskForNextFile()
+        {
+            DialogResult result = MessageBox.Show(new Form() { TopMost = true }, "Run again for another file?", "Completed!", MessageBoxButtons.YesNo);
+            return result == DialogResult.Yes;
+        }
+
+
+        [STAThread]
         static void Main(string[] args)
         {
+            //XDD But need to Message box to be appeared
+            Form form = new Form();
+            form.Load += (a, b) => form.Close();
+            Application.Run(form);
+
+
             if (args.Length == 1)
             {
                 GPXConverter.ConvertGpx(args[0]);
                 return;
             }
-
-            string inputPath;
-            Console.WriteLine("Enter input path:");
-            inputPath = Console.ReadLine();
-            inputPath = inputPath.Trim('\"');
-            GPXConverter.ConvertGpx(inputPath);
-
-
-            Console.WriteLine("press any key to continue...");
-            Console.ReadKey();
+            string inputPath = "";
+            do
+            {
+                try
+                {
+                    inputPath = OpenDialog();
+                    GPXConverter.ConvertGpx(inputPath);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            while (AskForNextFile());
         }
     }
 }
